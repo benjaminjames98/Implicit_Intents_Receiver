53723a251aee4dcaadc0d550bce19c760ae2220f
Ben <benjamin.james98@gmail.com>
2020-03-12 22:21:11 +1100

    Initial commit

---
+++ app/src/main/java/tech/bencloud/receiver/implicitintentsreceiver/MainActivity.java
@@ -0,0 +1,25 @@
+package tech.bencloud.receiver.implicitintentsreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String uriString = uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uriString);
+        }
+    }
+}
